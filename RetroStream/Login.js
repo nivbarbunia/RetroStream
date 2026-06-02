9cc652266c801756273c9f80cb206c6c4b6f995f
nivbarbunia <nivtapuzbar@gmail.com>
2026-06-02 15:26:46 +0300

    ex 2 login.js + css touchups

--- RetroStream/Login.js
+++ RetroStream/Login.js
@@ -1,15 +1,41 @@
 const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
 const message = document.getElementById("message");
 const form = document.getElementById("myform");
 
-emailInput.addEventListener("input", function () {
-    console.log(emailInput.value);
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    if (email.length === 0) {
+        emailInput.classList.add("input-error");
+        message.textContent = "לא הוזנה כתובת אימייל";
+        return;
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+        emailInput.classList.add("input-error");
+        message.textContent = "נסו להשתמש בפורמט yourname@email.com";
+        return;
+    }
+    if (password.length === 0) {
+        passwordInput.classList.add("input-error");
+        message.textContent = "לא הוזנה סיסמא";
+        return;
+    }
+    if (password.length < 6) {
+        passwordInput.classList.add("input-error");
+        message.textContent = "הסיסמה חייבת להכיל לפחות 6 תווים";
+        return;
+    }
+
+    window.location.href = "ProfilesScreen.html";
+
 });
 
-
-emailInput.addEventListener("input", function () {
-    message.textContent = emailInput.value;
+form.addEventListener("input", function (event) {
+    emailInput.classList.remove("input-error");
+    passwordInput.classList.remove("input-error");
+    message.textContent = "";
 });
-
-
-
