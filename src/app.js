fa2d3b4b2e7aff4cc0ea573ee0dbc8f919d74c29
nivbarbunia <nivtapuzbar@gmail.com>
2026-07-11 22:27:29 +0300

    sensitive information hiding

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(session({
-    secret: "retrostream_secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 } // one hour
